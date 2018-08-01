91168bd459c49b44e26a3e20aa1c48a5340e6bf1
Roy <luogao_lg@sina.com>
2018-08-01 18:58:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## ts-learning
+
+## Re.
+
+  - https://www.tslang.cn/docs/handbook/react-&-webpack.html
+
+  - https://www.webpackjs.com/guides/getting-started/
+
---
+++ gulp/gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const watchify = require('watchify')
+const tsify = require("tsify")
+const gutil = require('gulp-util')
+
+const paths = {
+  pages: ['src/*.html']
+}
+
+const watchedBrowserify = watchify(browserify({
+  basedir: '.',
+  debug: true,
+  entries: ['src/main.ts'],
+  cache: {},
+  packageCache: {}
+}).plugin(tsify))
+
+function bundle() {
+  return watchedBrowserify.bundle().pipe(source('bundle.js')).pipe(gulp.dest('dist'))
+}
+
+gulp.task('copy-html', function () {
+  return gulp.src(paths.pages).pipe(gulp.dest('dist'))
+})
+
+gulp.task('default', ['copy-html'], bundle)
+watchedBrowserify.on('update', bundle)
+watchedBrowserify.on('log', gutil.log)
